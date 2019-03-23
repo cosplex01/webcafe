3ea2d28cca2e37c0ee4ddeee55b0dc685d073dc5
cosplex01 <asukak@naver.com>
2019-03-23 16:31:14 +0900

    메인메뉴와 텝매뉴 제어를 위한 스크립트 완성

---
+++ js/webcafe.js
@@ -0,0 +1,22 @@
+var section = $('.board section');
+var tab = $('.tab');
+var boardLi = $('.board li');
+var menuLi = $('.menu > li');
+var sublink = $('.sub-menu a');
+
+boardLi.addClass('icon-dot-circled');
+sublink.addClass('icon-dot-circled');
+//메인메뉴의 하위메뉴를 제어하기 위한 스크립트
+menuLi.on('mouseover focusin',function(){
+  menuLi.removeClass('menu-act');
+  $(this).addClass('menu-act');
+});
+//공지사항 및 자료실 탭 제어를 위한 스크립트
+tab.on('click keyup',function(e){
+  e.preventDefault();
+  if(e.type == 'click' || e.keyCode == 13){
+    section.removeClass('board-act');
+    $(this).parent().addClass('board-act');
+  }
+  
+});
